66b3de2908c07905c9da95faaf89f06553c43d96
revam <noreply.git@revam.dev>
2026-04-12 04:47:28 +0200

    fix: remove incorrect help parameter from has tag by id expression

--- Shoko.Server/Filters/Info/HasTagByIDExpression.cs
+++ Shoko.Server/Filters/Info/HasTagByIDExpression.cs
@@ -1,8 +1,6 @@
 using System;
-using System.Linq;
 using Shoko.Abstractions.Filtering;
 using Shoko.Server.Filters.Interfaces;
-using Shoko.Server.Repositories;
 
 namespace Shoko.Server.Filters.Info;
 
@@ -16,7 +14,6 @@ public class HasTagByIDExpression : FilterExpression<bool>, IWithStringParameter
 
     public string Parameter { get; set; }
     public override string HelpDescription => "This condition passes if any of the anime have the specified AniDB tag by ID";
-    public override string[] HelpPossibleParameters => RepoFactory.AniDB_Tag.GetAllForLocalSeries().Select(a => a.TagName.Replace('`', '\'')).ToArray();
 
     public override bool Evaluate(IFilterableInfo filterable, IFilterableUserInfo userInfo, DateTime? time)
     {
