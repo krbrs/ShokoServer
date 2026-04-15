cff932fcb2f5e6e10e362803a845cb92a961b340
revam <noreply.git@revam.dev>
2026-04-15 19:14:19 +0200

    feat: add unique hash key to `AVDumpFilesJob`
    Added a new `Key` property that generates an MD5 hash of the videos dictionary to add the missing job identification in the job system. Also reduced the concurrency limit from 2 to 1.

--- Shoko.Server/Scheduling/Jobs/AniDB/AVDumpFilesJob.cs
+++ Shoko.Server/Scheduling/Jobs/AniDB/AVDumpFilesJob.cs
@@ -1,7 +1,12 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using Shoko.Server.Scheduling.Acquisition.Attributes;
+using Shoko.Server.Scheduling.Attributes;
 using Shoko.Server.Scheduling.Concurrency;
 using Shoko.Server.Utilities;
 
@@ -9,10 +14,25 @@ namespace Shoko.Server.Scheduling.Jobs.AniDB;
 
 [DatabaseRequired]
 [NetworkRequired]
-[LimitConcurrency(2, 16)]
+[LimitConcurrency(1, 16)]
 public class AVDumpFilesJob : BaseJob<AVDumpHelper.AVDumpSession>
 {
+    /// <summary>
+    /// Videos to dump.
+    /// </summary>
     public Dictionary<int, string> Videos { get; set; }
+
+    /// <summary>
+    /// Hash key representing the videos to dump.
+    /// </summary>
+    [JobKeyMember]
+    public string Key
+    {
+        get => Videos is not null
+            ? Convert.ToHexString(MD5.HashData(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(Videos.OrderBy(a => (a.Key, a.Value)).ToDictionary()))))
+            : string.Empty;
+        set { }
+    }
 
     public override string Title => "AVDumping Files";
     public override string TypeName => "AVDump Files";
